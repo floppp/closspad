40acc8e54fdbd27d650e4461b71b01095fff0d02
Fernando <fernandolaso@duck.com>
2025-05-19 20:09:26 +0200

    wip: some advance in reitit router

--- src/qoback/closspad/core.cljs
+++ src/qoback/closspad/core.cljs
@@ -1,6 +1,7 @@
 (ns qoback.closspad.core
   (:require [clojure.walk :as walk]
-            [replicant.dom :as r-dom]))
+            [replicant.dom :as r-dom]
+            [qoback.closspad.router :as router]))
 
 (defonce ^:private !state (atom {}))
 (defonce ^:private !dispatcher (atom {}))
@@ -15,26 +16,9 @@
 (defn ^{:dev/after-load true :export true} start! []
   (render! !state))
 
-(defn- enrich-action-from-event [{:replicant/keys [js-event node]} actions]
-  (walk/postwalk
-   (fn [x]
-     (cond
-       (keyword? x)
-       (case x
-         :event/target.value (-> js-event .-target .-value)
-         :dom/node node
-         x)
-       :else x))
-   actions))
 
-(defn- enrich-action-from-state [state action]
-  (walk/postwalk
-   (fn [x]
-     (cond
-       (and (vector? x)
-            (= :db/get (first x))) (get state (second x))
-       :else x))
-   action))
+
+
 
 (defn navigated-home-page []
   (let [dispatcher (get-dispatcher)]
@@ -44,25 +28,41 @@
   (let [dispatcher (get-dispatcher)]
     (dispatcher nil [[:db/assoc :page/navigated {:page :home}]])))
 
-(defn- event-handler [{:replicant/keys [^js js-event] :as replicant-data} actions]
-  (doseq [action actions]
-    (let [enriched-action (->> action
-                               (enrich-action-from-event replicant-data)
-                               (enrich-action-from-state @!state))
-          [action-name & args] enriched-action]
-      (case action-name
-        :dom/prevent-default (.preventDefault js-event)
-        :db/assoc (apply swap! !state assoc args)
-        :db/assoc-in (apply swap! !state assoc-in args)
-        :db/dissoc (apply swap! !state dissoc args)
-        :route/home (navigated-home-page)
-        :route/match (navigated-match-page (assoc (second enriched-action) :state @!state))
-        ;; :route/classification (navigated-product-page (assoc (second enriched-action) :state @!state))
-        ;; :route/login (navigated-new-product-page (assoc (second enriched-action) :state @!state))
-        )))
-  (render! @!state))
+(defn- handle-action [state replicant-data action]
+  (let [[action-name & args] action]
+    (case action-name
+      :dom/prevent-default {:effects [[:dom/fx.prevent-default]]} ;;(.preventDefault js-event)
+      :db/assoc {:new-state (apply swap! !state assoc args)}
+      :db/assoc-in {:new-state (apply swap! !state assoc-in args)}
+      :db/dissoc {:new-state (apply swap! !state dissoc args)}
+      :route/home {:effects [[:route/home]]}
+      :route/match {:effects [[:route/match {:id 1}]]}
+      ;; :route/classification (navigated-product-page (assoc (second enriched-action) :state @!state))
+      ;; :route/login (navigated-new-product-page (assoc (second enriched-action) :state @!state))
+      )))
 
-#_(defn watch! [render!]
+(defn handle-actions [state replicant-data actions]
+  (reduce (fn [{state :new-state :as acc} action]
+            (let [{:keys [new-state effects]} (handle-action state replicant-data action)]
+              (cond-> acc
+                new-state (assoc :new-state new-state)
+                effects (update :effects into effects))))
+          {:new-state state
+           :effects []}
+          actions))
+
+(defn- event-handler [replicant-data actions]
+  (let [{:keys [new-state effects]} (handle-actions @!state replicant-data actions)]
+    (when new-state
+      (reset! !state new-state))
+    (when effects
+      (doseq [effect effects]
+        (when js/goog.DEBUG (js/console.debug "Triggered effect" effect))
+        ;;(effects/perform-effect! replicant-data effect)
+        
+        ))))
+
+(defn watch! [render!]
     (add-watch !state
                ::render (fn [_ _ old-state new-state]
                           (when (not= old-state new-state)
@@ -71,5 +71,6 @@
 (defn ^:export init! []
   (r-dom/set-dispatch! event-handler)
   (swap! !dispatcher assoc :dispatcher event-handler)
-  ;; (watch! render!)
+  (router/start! router/routes event-handler)
+  (watch! render!)
   (start!))
--- src/qoback/closspad/pages/views.cljc
+++ src/qoback/closspad/pages/views.cljc
@@ -6,4 +6,4 @@
    [:div.flex-grow.p-4
     [:div.flex.flex-col.items-center.min-h-screen.mt-10
 
-     (page-content state)]]])
+     ]]])
