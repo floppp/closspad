200633fa7be37a4593db7261b2ffaf8bcd00ceea
fernando <fernandolaso@duck.com>
2025-05-24 22:13:49 +0200

    feat: using enrichted-{actions,state}

--- src/qoback/closspad/state/events.cljs
+++ src/qoback/closspad/state/events.cljs
@@ -1,10 +1,34 @@
 (ns qoback.closspad.state.events
-  (:require [qoback.closspad.state.db :refer [!state]]
-            [qoback.closspad.state.effects :refer [perform-effect!]]))
+  (:require [clojure.walk :as walk]
+            [qoback.closspad.state.db :refer [!state]]
+            [qoback.closspad.state.effects :refer [perform-effect!]]
+            [qoback.closspad.utils :as u]))
+
+(defn- enrich-action-from-event [{:replicant/keys [js-event node]} actions]
+  (walk/postwalk
+   (fn [x]
+     (cond (keyword? x)
+           (case x
+             :event/target.value (-> js-event .-target .-value)
+             :dom/node node
+             x)
+           :else x))
+   actions))
+
+(defn- enrich-action-from-state [state action]
+  (walk/postwalk
+   (fn [x]
+     (cond (and (vector? x) (= :db/get (first x)))
+           (get state (second x))
+           :else x))
+   action))
 
 (defn- handle-event
   [state replicant-data event]
-  (let [[action-name & args] event]
+  (let [enrichted-event (->> event
+                             (enrich-action-from-event replicant-data)
+                             (enrich-action-from-state state))
+        [action-name & args] enrichted-event]
     (case action-name
       :dom/prevent-default {:effects [[:dom/fx.prevent-default]]} ;;(.preventDefault js-event)
       :db/assoc {:new-state (apply assoc state args)}
@@ -13,8 +37,8 @@
       :route/home {:effects [[:route/home]]}
       :route/match {:effects [[:route/match args]]}
       :data/query {:effects [[:data/query {:state state :args args}]]}
-      (.log js/console "Unknown event " action-name "with arguments" args )
-      )))
+      (when goog.DEBUG
+        (.log js/console "Unknown event " action-name "with arguments" args)))))
 
 (defn- handle-events
   [state replicant-data events]
@@ -27,11 +51,13 @@
    {:new-state state :effects []}
    events))
 
-(defn event-handler [replicant-data actions]
-  (let [{:keys [new-state effects]} (handle-events @!state replicant-data actions)]
+(defn event-handler
+  [replicant-data events]
+  (let [{:keys [new-state effects]} (handle-events @!state replicant-data events)]
     (when new-state
       (reset! !state new-state)
-      (.log js/console "New state" @!state))
+      (when goog.DEBUG
+        (.log js/console ">>>>> new state" @!state)))
     (when effects
       (doseq [effect effects]
         (perform-effect! replicant-data effect)))))
