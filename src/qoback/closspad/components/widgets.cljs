f85517b3efc749c9e1756f76427bbe359f8b57b0
fernando <fernandolaso@duck.com>
2025-07-07 22:01:23 +0200

    fix: arrow buttons date width

--- src/qoback/closspad/components/widgets.cljs
+++ src/qoback/closspad/components/widgets.cljs
@@ -52,7 +52,7 @@
 
    [:div
     {:class
-     ["text-lg" "font-semibold" "min-w-120" "text-center" "sm:text-xl"]}
+     ["text-lg" "font-semibold" "min-w-60" "text-center" "sm:text-xl"]}
     (dt/datetime->date->str date)]
 
    (arrow-right date match-dates)
