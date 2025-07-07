3adf72c67761b34aa769475e70fdaefe4b07aaaa
fernando <fernandolaso@duck.com>
2025-07-07 21:59:59 +0200

    feat: updating deps and fixing make & dialog style
    Because of new tailwind and daisyui

--- src/qoback/closspad/components/dialog.cljs
+++ src/qoback/closspad/components/dialog.cljs
@@ -11,29 +11,30 @@
     [:div {:style {:position "fixed"
                    :width "100vw"
                    :height "100vh"
-                   :display "grid"
-                   :place-items "center"
+                   :display "flex"
                    :background "rgba(0, 0, 0, 0.5)"
                    :backdrop-filter "blur(5px)"
                    :overflow "hidden"
                    :top 0
-                   :left 0}
+                   :left 0
+                   :z-index "10"
+                   :align-items "center"
+                   }
            :on {:click  [[:db/dissoc :ui/dialog]]}}
-     [:dialog
+     [:dialog.rounded-lg
       {:open opened?
-       :style {:border "none"
-               :padding "20px"
-               :box-shadow "0 4px 12px rgba(0, 0, 0, 0.15)"
-               :background "white"
+       :style {:padding "20px"
                :max-height "75vh"
                :overflow "auto"
-               :z-index "100"}}
+               :margin "auto"}}
+
       (when title [:h2.mb-4.pb-2 title])
-      (map
-       (fn [[k v]]
-         [:p.flex.justify-between.gap-8.mb-2
-          [:span (name k)]
-          [:span v]])
-       (filter (fn [[k _]] (not= k :id)) info))
+      [:div
+       (map
+        (fn [[k v]]
+          [:p.flex.justify-between.gap-8.mb-2
+           [:span (name k)]
+           [:span v]])
+        (filter (fn [[k _]] (not= k :id)) info))]
       (when extra-node
         extra-node)]]))
