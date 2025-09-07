1d6468a1cb8d5a346947e6fdfbe10fde1b85674e
fernando <fernandolaso@duck.com>
2025-09-07 09:14:14 +0200

    fix: Merge issues

--- src/qoback/closspad/utils/numbers.cljs
+++ src/qoback/closspad/utils/numbers.cljs
@@ -4,15 +4,9 @@
   [n]
   (/ (Math/round (* 2 n)) 2))
 
-<<<<<<< HEAD
 (defn to-fixed-num
   "Number to Number, using the number of decimals
   specified. "
   [n decimals]
   (js/parseFloat (.toFixed n decimals)))
-=======
-(defn to-fixed-num [n decimals]
-  (when n
-    (js/parseFloat (.toFixed n decimals))))
->>>>>>> e4368b573f183a0676772f7d1a2005a3df3538fc
 
